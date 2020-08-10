ed92973fe7e27c352899783cad2be16ddd425cf9
Luciano Frizzera <lucaju@gmail.com>
2020-08-09 22:00:28 -0400

    style: reinforce some linting rules

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,10 @@ module.exports = {
 	apps: [
 		{
 			name: 'cwrc-gitserver',
-            script: './bin/www',
-            args: '--no-daemon',
-			env: {
-				NODE_ENV: 'production',
-			},
-			env_production: {
-				NODE_ENV: 'production',
-			},
+			script: './bin/www',
+			args: '--no-daemon',
+			env: { NODE_ENV: 'production' },
+			env_production: { NODE_ENV: 'production' },
 		},
 	],
 };
